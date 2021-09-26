47338d9d8f1eaf6ad825d22f8d6680172ffc57e1
Mauricio Corredor <malec@uniandes.edu.co>
2021-09-26 12:06:54 -0500

    fix de GF01 26092021

--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -37,6 +37,7 @@ class VistaCancion(Resource):
         cancion.minutos = request.json.get("minutos",cancion.minutos)
         cancion.segundos = request.json.get("segundos",cancion.segundos)
         cancion.interprete = request.json.get("interprete",cancion.interprete)
+        cancion.favorita = request.json.get("favorita",cancion.favorita)
         cancion.genero = request.json.get("genero", cancion.genero)
         db.session.commit()
         return cancion_schema.dump(cancion)
@@ -120,7 +121,7 @@ class VistaCancionesAlbum(Resource):
             else:
                 return 'Canción errónea',404
         else:
-            nueva_cancion = Cancion(titulo=request.json["titulo"], minutos=request.json["minutos"], segundos=request.json["segundos"], interprete=request.json["interprete"])
+            nueva_cancion = Cancion(titulo=request.json["titulo"], minutos=request.json["minutos"], segundos=request.json["segundos"], interprete=request.json["interprete"], favorita= request.json["favorita"])
             album.canciones.append(nueva_cancion)
         db.session.commit()
         return cancion_schema.dump(nueva_cancion)
